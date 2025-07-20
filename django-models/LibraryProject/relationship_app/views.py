7d422fe4bc47c06ed3007699c93bd4922df0cb95
Bigwills <ajbodesecond@gmail.com>
2025-07-20 01:42:21 +0100

    Fix Task 1: Django Views and URL Configuration - Fixed function-based and class-based views with proper templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,19 +14,14 @@ from django.contrib.auth.decorators import permission_required
 #Function-based views
 def list_books(request):
     books = Book.objects.all() #fetching all books from the database
-    context = {'list_books':books} #creates a context dictionary with list of books
+    context = {'books':books} #creates a context dictionary with list of books
     return render(request, 'relationship_app/list_books.html', context)
 
 #class-based view for listing books in a library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        library = self.get_object()
-        context['books_list'] = library.get_books_list()
-        return context
+    context_object_name = 'library'
     
 
 #Setup User Authentication Views
